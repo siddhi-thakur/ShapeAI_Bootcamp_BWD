88a81c60001d2f573d2cb64eb64762afd49bdcf5
siddhi-thakur <85453618+siddhi-thakur@users.noreply.github.com>
2021-06-07 00:06:26 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript with React.js</h1>
+        <p>
+          this is a good bootcamp for learning Javascript and React.js for
+          beginners.
+        </p>
+      </div>
+
+      <div className="note">
+        <h1>Python</h1>
+        <p>
+          this is a good bootcamp for learning Python Programming Language for
+          beginners. And improving problem solving skills.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
